2ececa92a8a7d8c953299a410e862d907e3dd7a9
Alhassane Seye <seyealhassane@gmail.com>
2023-11-27 19:55:31 +0100

    formulaire configure

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/send-email', (req, res) => {
+  const { nom, email, objet, message } = req.body;
+
+  // Créer un transporteur pour envoyer des emails
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'seyealhassane@gmail.com',
+      pass: 'yicjkelfeenfgfoi', // Remplacez par votre mot de passe d'application
+    },
+  });
+  
+
+  // Définir les options de l'email
+  const mailOptions = {
+    from: email,
+    to: 'seyealhassane@gmail.com',
+    subject: objet,
+    text: `Nom: ${nom}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  // Envoyer l'email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.status(200).json({ message: 'Email envoyé : ' + info.response });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+});
--- src/components/jsx/fromContact.jsx
+++ src/components/jsx/fromContact.jsx
@@ -1,16 +1,45 @@
 import '../style/formcontact.sass'
+import React, { useState } from 'react';
 
 function FormContact() {
-    return(
-    <form id="formulaireContact">
-        <h3>Envoyez un mail</h3>
-        <input type="text" placeholder="Nom" id="prenom" required="required"/>
-        <input type="email" placeholder="Email" id='email'/>
-        <input type="text" placeholder="Objet" id="objet" required="required"/>
-        <textarea name="message" placeholder="Votre message..." id="message" required="required"/>
-        <input type="submit" value="Envoyer" id='bouton'/>
+  const [nom, setNom] = useState('');
+  const [email, setEmail] = useState('');
+  const [objet, setObjet] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Envoyer les données du formulaire au serveur
+    const response = await fetch('http://localhost:3001/send-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ nom, email, objet, message }),
+      });
+      
+      const result = await response.text();
+      console.log(result);      
+  };
+
+  return (
+    <form id="formulaireContact" onSubmit={handleSubmit}>
+      <h3>Envoyez un mail</h3>
+      <input type="text" placeholder="Nom" id="prenom" value={nom} onChange={(e) => setNom(e.target.value)} required />
+      <input type="email" placeholder="Email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+      <input type="text" placeholder="Objet" id="objet" value={objet} onChange={(e) => setObjet(e.target.value)} required />
+      <textarea
+        name="message"
+        placeholder="Votre message..."
+        id="message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        required
+      />
+      <input type="submit" value="Envoyer" id="bouton" />
     </form>
-    )
+  );
 }
 
-export default FormContact
+export default FormContact;
